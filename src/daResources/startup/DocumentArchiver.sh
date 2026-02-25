730bee996b730fe9095408b98c68cbebd60698a9
Stephan Sann <stephan@sann.name>
2026-02-25 06:37:41 +0100

    Added support for JavaFX aarch64, updated dependencies.

--- src/daResources/startup/DocumentArchiver.sh
+++ src/daResources/startup/DocumentArchiver.sh
@@ -1,1 +1,1 @@
-java --module-path lib --module DocumentArchiver/com.sophisticatedapps.archiving.documentarchiver.Launcher "$1"
+java --enable-native-access=javafx.graphics,com.sun.jna --module-path lib --module DocumentArchiver/com.sophisticatedapps.archiving.documentarchiver.Launcher "$1"
